7a3adcf79b53f4be670a63a32fc2cc127e0052ad
acwilliammsn <thelordl2@hotmail.com>
2024-10-20 12:19:02 -0300

    chore: abrindo camera

--- src/app/modais/check-in-modal/check-in-modal.component.ts
+++ src/app/modais/check-in-modal/check-in-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 
@@ -16,23 +16,33 @@ interface Child {
 })
 export class CheckInModalComponent {
   selectedChildId: number | null = null;
+  showCamera: boolean = false;
+  stream: MediaStream | null = null;
+  @ViewChild('videoElement') videoElement!: ElementRef;
   constructor(
     public dialogRef: MatDialogRef<CheckInModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { children: Child[] }
   ) {}
 
   onCancel(): void {
+    this.stopCamera()
     this.dialogRef.close();
   }
 
   onConfirm(): void {
 
     const selectedChildrenIds = this.data.children
-    .filter(child => child.selecionado) // Filtra as crianças que estão selecionadas
-    .map(child => child.id); // Mapeia para obter apenas os IDs
-  // Faça algo com os IDs das crianças selecionadas
+    .filter(child => child.selecionado)
+    .map(child => child.id);
+
     console.log('crianças selecionadas para checkin',selectedChildrenIds);
-    this.dialogRef.close(this.selectedChildId);
+    if (!this.showCamera) {
+      this.openCamera();
+    } else {
+      this.capturePhoto();
+      this.stopCamera();
+      this.dialogRef.close(selectedChildrenIds);
+    }
   }
 
   hasSelectedChildren(): boolean {
@@ -42,5 +52,41 @@ export class CheckInModalComponent {
   updateSelectedChildren(child: any) {
     console.log('criança selecionada', child.nomeCrianca)
     child.selected = !child.selected;
+  }
+
+  async openCamera(): Promise<void> {
+    try {
+      const constraints = {
+        video: {
+          facingMode: { exact: "environment" }
+        }
+      };
+      this.stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.videoElement.nativeElement.srcObject = this.stream;
+      this.showCamera = true;
+    } catch (err) {
+      console.error('Erro ao abrir a câmera:', err);
+      // Se falhar ao abrir a câmera traseira, tente abrir qualquer câmera disponível
+      try {
+        this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.videoElement.nativeElement.srcObject = this.stream;
+        this.showCamera = true;
+      } catch (fallbackErr) {
+        console.error('Erro ao abrir qualquer câmera:', fallbackErr);
+      }
+    }
+  }
+
+  stopCamera(): void {
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => track.stop());
+      this.stream = null;
+    }
+    this.showCamera = false;
+  }
+
+  capturePhoto(): void {
+    // Implementar a lógica para capturar a foto
+    console.log('Foto capturada');
   }
 }
