d12b115fdf698a0ebc3923e0bf672a546f5108c3
acwilliammsn <thelordl2@hotmail.com>
2025-03-22 20:24:19 -0300

    chore: alterando layout pagina principal

--- src/app/componentes/pagina-principal/pagina-principal.component.ts
+++ src/app/componentes/pagina-principal/pagina-principal.component.ts
@@ -26,6 +26,7 @@ export class PaginaPrincipalComponent  {
   role: string = '';
   menuItems: MenuItem[] = [
     { label: 'cadastro', route: '/cadastro', icon: 'home', visible: true },
+    { label: 'grupos', route: '/cadastro', icon: 'home', visible: true },
     { label: 'Crianças', route: '/lista-crianca', icon: 'shopping_basket', visible: this.isAdmin || this.isVoluntario },
     { label: 'sair', callback: () => this.logout(), icon: 'exit_to_app', visible: true },
   ];
@@ -67,15 +68,17 @@ export class PaginaPrincipalComponent  {
 
   private atualizarMenuItems(email: string) {
     this.menuItems = [
-      { label: 'cadastro', route:`/cadastro/${email}`, icon: 'home', visible: true },
-      { label: 'perfil-usuario', route: '/usuario-kids', icon: 'home', visible: true },
-      { label: 'disponibilidade', route: '/disponibilidade', icon: 'home', visible: true },
-      { label: 'cadastrar função', route: '/cadastrar-funcao', icon: 'home', visible: true },
-      { label: 'Reuniões', route: '/reunioes', icon: 'home', visible: true },
-      { label: 'Crianças', route: '/lista-crianca', icon: 'shopping_basket', visible: this.isAdmin || this.isVoluntario },
+      { label: 'Cadastro Criança', route: `/cadastro/${email}`, icon: 'home', visible: true },
+      { label: 'Grupos', icon: 'groups',   route: '/adicionar-grupo',  visible: true     },
+      { label: 'User', route: '/usuario-kids', icon: 'person', visible: true },
+      { label: 'disponibilidade', route: '/disponibilidade', icon: 'event_available', visible: true },
+      { label: 'Funções', route: '/cadastrar-funcao', icon: 'add_circle_outline', visible: true },
+      { label: 'Reuniões', route: '/reunioes', icon: 'event', visible: true },
+      { label: 'Crianças', route: '/lista-crianca', icon: 'child_care', visible: this.isAdmin || this.isVoluntario },
       { label: 'sair', callback: () => this.logout(), icon: 'exit_to_app', visible: true },
     ];
   }
+
 
 }
 
